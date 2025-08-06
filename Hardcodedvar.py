1ffe583a20fedc9030c9d112d939c4ff2a332b4b
ChintagumpalaAkshaya <cakshaya0418@gmail.com>
2025-08-06 15:13:31 +0530

    second commit

---
+++ Hardcodedvar.py
@@ -0,0 +1,8 @@
+name = "Akshaya"        
+age = 20               
+height = 5.7            
+is_student = True       
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student?", is_student)
